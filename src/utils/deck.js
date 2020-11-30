0259f097def74d895649c4fc24c27f0e52e55592
Marcus Couto <markitovc@gmail.com>
2020-11-30 16:56:12 -0300

    update generateCards

--- src/utils/deck.js
+++ src/utils/deck.js
@@ -1,13 +1,26 @@
 import deepcopy from "deepcopy";
 import { v4 as uuidv4 } from "uuid";
 
+
+const cardsList = [
+  "macaco.png",
+  "porquinho.png",
+  "tartaruga.png",
+  "tubarao.png",
+  "canguru.png",
+  "crocodilo.png",
+];
+
+  const totalCards = cardsList.length * 2;
+
+
 export const shuffleArray = (array) => {
   return array.sort(() => 0.5 - Math.random());
 };
 
-export const generateCards = (count, cardsList) => {
+export const generateCards = () => {
   const cards = shuffleArray(cardsList)
-    .slice(0, count / 2)
+    .slice(0, totalCards / 2)
     .map((imageURL) => ({
       id: uuidv4(),
       imageURL: "../images/" + imageURL,
